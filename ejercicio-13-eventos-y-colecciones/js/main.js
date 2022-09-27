876fc6c348940525648dd7c5ae21275deb4360e6
Emanuel Some <em7som@gmail.com>
2022-09-27 19:13:07 -0300

    Inicio punto 4

--- ejercicio-13-eventos-y-colecciones/js/main.js
+++ ejercicio-13-eventos-y-colecciones/js/main.js
@@ -8,9 +8,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   /* Punto 3 */
-  let textArea = document.getElementById('origen')
+  const textArea = document.getElementById('origen')
   //console.dir(textArea)
   textArea.value = `<p>Este contenido <strong>está listo</strong><br>para ser editado y pasarlo abajo.</p>`
+
+
+  /* Punto 4 */
+
+  textArea.addEventListener('input', () => {
+    const coleccionInput = document.getElementsByTagName('input')
+    console.log(coleccionInput)
+
+/*     coleccionInput.forEach(inputx => {
+      inputx.disabled = 'false'
+    }); */
+  })
 
   })  
 
