aded9512356edd7cae1465fbd2506ba9179c862c
Emanuel Some <em7som@gmail.com>
2022-09-27 18:41:08 -0300

    Agrego punto 3

--- ejercicio-13-eventos-y-colecciones/js/main.js
+++ ejercicio-13-eventos-y-colecciones/js/main.js
@@ -1,15 +1,23 @@
 
-/* Punto 2 */
-
-/* document.addEventListener("DOMContentLoaded", () => {
-    console.log("Contenido del DOM cargado")
-  }) */
 
 
-/* Punto 3 */
+document.addEventListener("DOMContentLoaded", () => {
+  
+  /* Punto 2 */
+  console.log("Contenido del DOM cargado")
 
-document.addEventListener("onload", () => {
-  let texta = document.getElementById('origen')
-  console.log(texta)
-})
+
+  /* Punto 3 */
+  let textArea = document.getElementById('origen')
+  //console.dir(textArea)
+  textArea.value = `<p>Este contenido <strong>está listo</strong><br>para ser editado y pasarlo abajo.</p>`
+
+  })  
+
+
+
+
+
+
+
 //textArea.innerHTML = "PRUEBa"
