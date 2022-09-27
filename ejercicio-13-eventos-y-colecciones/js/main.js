0f35530b27110a1ea8c6e0e0be921794351ed6a8
Emanuel Some <em7som@gmail.com>
2022-09-27 16:31:37 -0300

    prueba

--- ejercicio-13-eventos-y-colecciones/js/main.js
+++ ejercicio-13-eventos-y-colecciones/js/main.js
@@ -1,13 +1,15 @@
 
 /* Punto 2 */
 
-document.addEventListener("DOMContentLoaded", () => {
+/* document.addEventListener("DOMContentLoaded", () => {
     console.log("Contenido del DOM cargado")
-  })
+  }) */
 
 
 /* Punto 3 */
 
-const textAre = window.document.getElementById('origen')
-console.log(textAre)
+document.addEventListener("onload", () => {
+  let texta = document.getElementById('origen')
+  console.log(texta)
+})
 //textArea.innerHTML = "PRUEBa"
