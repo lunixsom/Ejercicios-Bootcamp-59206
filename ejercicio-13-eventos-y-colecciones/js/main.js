8f76b2874d294d60f1facb03adefc6872029daa5
Emanuel Some <em7som@gmail.com>
2022-10-03 15:43:44 -0300

    Punto 5 - Agrego funcionalidad boton Reemplazar

--- ejercicio-13-eventos-y-colecciones/js/main.js
+++ ejercicio-13-eventos-y-colecciones/js/main.js
@@ -1,26 +1,40 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
+  const textArea = document.getElementById("origen");
+  const coleccionInput = document.querySelectorAll("input");
+  const botonAMinusculas = document.querySelector('button')
+  const divDestino = document.getElementById('destino')
+
   /* Punto 2 */
   console.log("Contenido del DOM cargado");
 
   /* Punto 3 */
-  const textArea = document.getElementById("origen");
   //console.dir(textArea)
   textArea.value = `<p>Este contenido <strong>está listo</strong><br>para ser editado y pasarlo abajo.</p>`;
 
   /* Punto 4 */
   textArea.addEventListener("input", () => {
-    const coleccionInput = document.querySelectorAll("input");
-    const botonAMinusculas = document.querySelector('button')
-    //console.log(coleccionInput)
-    
     botonAMinusculas.disabled = false
 
     coleccionInput.forEach((input) => {
       input.disabled = false
     });
   });
+  
+  /* Punto 5 */
+
+  function agregarNvecesADivDestino(cant = 1){
+    let contenidoOrigen = textArea.value
+    divDestino.innerHTML = contenidoOrigen
+  }
+
+  coleccionInput[0].addEventListener('click', () => {
+    agregarNvecesADivDestino()
+  })
 });
 
-/* Punto 5 */
+
+
 
 
